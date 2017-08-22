3b6f449e010df7b2a5d73c918e5fdd6b161ab4ec
Thiago Arcanjo <thiagokai.arcanjo@gmail.com>
2017-08-22 02:31:48 -0300

    Alteração no controller Home

--- 3dbrasil/application/controllers/Home.php
+++ 3dbrasil/application/controllers/Home.php
@@ -4,52 +4,35 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
 
 	public function index(){
-		$this->load->view('html-header');
-		$this->load->view('header');
-		$this->load->view('home');
-		$this->load->view('footer');
-		$this->load->view('html-footer');
+		$this->carregarViews('home');
 	}
 
 	public function sobre(){
-		$this->load->view('html-header');
-		$this->load->view('header');
-		$this->load->view('sobre');
-		$this->load->view('footer');
-		$this->load->view('html-footer');
+		$this->carregarViews('sobre');
 	}
 
 	public function parceiros(){
-		$this->load->view('html-header');
-		$this->load->view('header');
-		$this->load->view('parceiros');
-		$this->load->view('footer');
-		$this->load->view('html-footer');
+		$this->carregarViews('parceiros');
 	}
 
 	public function produtos(){
-		$this->load->view('html-header');
-		$this->load->view('header');
-		$this->load->view('produtos');
-		$this->load->view('footer');
-		$this->load->view('html-footer');
+		$this->carregarViews('produtos');
 	}
 
 	public function contato(){
-		$this->load->view('html-header');
-		$this->load->view('header');
-		$this->load->view('contato');
-		$this->load->view('footer');
-		$this->load->view('html-footer');
+		$this->carregarViews('contato');
 	}
 
 	public function error404(){
-		
+		$this->carregarViews('error404');
+
+	}
+
+	public function carregarViews($view){
 		$this->load->view('html-header');
 		$this->load->view('header');
-		$this->load->view('error404');
+		$this->load->view($view);
 		$this->load->view('footer');
 		$this->load->view('html-footer');
 	}
-
 }
