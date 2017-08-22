d1f2f737ec2e79d7d010dafaba1be31ab148d73d
Thiago Arcanjo <thiagokai.arcanjo@gmail.com>
2017-08-22 01:48:39 -0300

    Adiciando helpers ao autoload, template, desmembramento de templete, criação de novas views e suas rotas e um controlador simples.

---
+++ 3dbrasil/application/views/footer.php
@@ -0,0 +1,82 @@
+<!--footer strat here-->
+<div class="footer">
+	<div class="container">
+		<div class="footer-main">
+			<div class="col-md-3 ftr-grids">
+				<div class="ftr-logo">
+				   <img src="images/logo.png" alt="">
+				</div>
+				<p>3D Brasil roto moldagem.</p>
+			</div>
+			<div class="col-md-3 ftr-grids">
+				<h3>Redes Sociais</h3>
+				<ul>
+
+					<li><a href="#">Facebook</a></li>
+					<li><a href="#">Instagran</a></li>
+				</ul>
+			</div>
+			<div class="col-md-3 ftr-grids">
+				<h3>Contatos</h3>
+				<form action="#" method="post">
+					<div class="input-typt-grid">
+						<div class="input-type-text">
+							<p>Name</p>
+						</div>
+						<div class="input-type-blocks">
+							<input type="text"  value="" name="Name" required="">
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="input-typt-grid">
+						<div class="input-type-text">
+							<p>Email</p>
+						</div>
+						<div class="input-type-blocks">
+							<input type="text"  value="" name="Email">
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="input-typt-grid">
+						<div class="input-type-text">
+							<p>Message</p>
+						</div>
+						<div class="input-type-blocks">
+							<textarea></textarea>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<input type="submit" value="Enviar">
+
+				</form>
+				<div class="clearfix"> </div>
+			</div>
+			<div class="col-md-3 ftr-grids">
+				<h3>Endereço</h3>
+				<p>Estrada do bom Tempo,nº41,Franco da Rocha-SP</p>
+			</div>
+		</div>
+	</div>
+</div>
+<!--footer end here-->
+<!--copy rights start here-->
+<div class="copy-right">
+	<div class="container">
+		 <div class="copy-rights-main wow zoomIn" data-wow-delay="0.3s">
+		 	<div class="copy-left">
+		 		<p>&copy; <?php echo date("Y"); ?> - Todos os direitos reservados</p>
+		 	</div>
+		 	<div class="copy-rit">
+    	      <ul class="ftr-navg">
+    	       	<li><a  href="<?php echo base_url('home'); ?>">Home </a></li>
+    	       	<li><a  href="<?php echo base_url('sobre'); ?>">Sobre</a></li>
+    	       	<li><a  href="<?php echo base_url('parceiros'); ?>">Parceiros</a></li>
+    	       	<li><a  href="<?php echo base_url('produtos'); ?>">Produtos</a></li>
+    	        <li><a  href="<?php echo base_url('contato'); ?>">Contato</a></li>
+    	       </ul>
+    	    </div>
+    	    <div class="clearfix"> </div>
+    	 </div>
+    </div>
+</div>
+<!--copy right end here-->
---
+++ 3dbrasil/application/views/sobre.php
@@ -0,0 +1,15 @@
+<div class="about">
+		<div class="container">
+			<div class="about-bottom">
+				<a href="single.html"><img src="images/ab.jpg" alt=""></a>
+				<h1><a href="single.html"><center> 3D Brasil</center></a></h1>
+				<p>Esta história é escrita por personagens que acreditaram 
+				em um sonho e o transformaram em realidade, levando a 
+				empresa e seus processos a serem reconhecidos no Brasil por empresas multinacionais.</p>
+
+				<p>Estamos em constante evolução de suas linhas de produtos, por isso busca cada
+				vez mais o desenvolvimento de itens que priorizem o design funcional, com foco em praticidade, beleza e bem-estar.</p>
+			</div>
+			<br> <br> <br>
+		</div>
+</div>
