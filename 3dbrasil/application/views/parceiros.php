9707b26e8f0a6230bc4db4b6a0d8d4b4826aec0a
Thiago Arcanjo <thiagokai.arcanjo@gmail.com>
2017-08-25 20:19:03 -0300

    Exemplo simples de parceiros

--- 3dbrasil/application/views/parceiros.php
+++ 3dbrasil/application/views/parceiros.php
@@ -1,7 +1,47 @@
 <div class="parceiros">
-		<div class="container">
-			<div class="about-bottom">
-				<h1>Parceiros</h1>
+	<div class="container">
+		<div class="about-bottom">
+			<h1 align="center">Parceiros </h1>
+			<br><br>
+			<div class="row">
+				<div class="col-md-3">
+					Parceiro 1
+				</div>
+				<div class="col-md-4">
+					<img src="<?php echo base_url('assets/images/ab.jpg') ?>" alt="">
+				</div>
+				<div class="col-md-5">
+					Parceiro tal tal Parceiro tal tal Parceiro tal tal  Parceiro tal tal
+				</div>
 			</div>
+			<br>
+			<div class="clear-fix"></div>
+			<div class="row">
+				<div class="col-md-3">
+					Parceiro 2
+				</div>
+				<div class="col-md-4">
+					<img src="<?php echo base_url('assets/images/ab.jpg') ?>" alt="">
+				</div>
+				<div class="col-md-5">
+					Parceiro tal tal Parceiro tal tal Parceiro tal tal  Parceiro tal tal
+				</div>
+			</div>
+			<br>
+			<div class="clear-fix"></div>
+			<div class="row">
+				<div class="col-md-3">
+					Parceiro 3
+				</div>
+				<div class="col-md-4">
+					<img src="<?php echo base_url('assets/images/ab.jpg') ?>" alt="">
+				</div>
+				<div class="col-md-5">
+					Parceiro tal tal Parceiro tal tal Parceiro tal tal  Parceiro tal tal
+				</div>
+			</div>
+			<br>
+			<div class="clear-fix"></div>
 		</div>
+	</div>
 </div>
